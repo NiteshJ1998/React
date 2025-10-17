449487fec51e23482f586cba2966b424c4e4e142
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2025-10-17 15:56:34 +0530

    adding react-routes

--- ReactRouting/src/App.jsx
+++ ReactRouting/src/App.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import Dashboard from "./components/Dashboard";
+import Home from "./components/Home";
+import About from "./components/About";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/about", element: <About /> },
+  { path: "/dashboard", element: <Dashboard /> },
+]);
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ ReactRouting/src/components/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Dashboard() {
+  return <div>DashboardPage</div>;
+}
+
+export default Dashboard;
