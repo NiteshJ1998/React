f6a0871a7077b7ab9225791ad414eb598cd108ba
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2025-10-17 20:08:48 +0530

    adding more changes

--- ReactRouting/src/App.jsx
+++ ReactRouting/src/App.jsx
@@ -3,11 +3,46 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 import Dashboard from "./components/Dashboard";
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+import ParamComp from "./components/ParamComp";
 
 const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/about", element: <About /> },
-  { path: "/dashboard", element: <Dashboard /> },
+  {
+    path: "/",
+    element: (
+      <div>
+        <Navbar />
+        <Home />
+      </div>
+    ),
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <Navbar />
+        <About />
+      </div>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <div>
+        <Navbar />
+        <Dashboard />
+      </div>
+    ),
+  },
+  {
+    path: "/students/:id",
+    element: (
+      <div>
+        <Navbar />
+        <ParamComp />
+      </div>
+    ),
+  },
 ]);
 
 function App() {
