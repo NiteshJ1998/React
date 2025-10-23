b23acf1dda25898a6f5393b021eab675cd3f565b
Nitesh Jaiswar <niteshjaiswar10@gmail.com>
2025-10-23 20:51:11 +0530

    adding redux-toolkit files

--- Redux-toolkit/src/App.jsx
+++ Redux-toolkit/src/App.jsx
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment } from "./features/counter/counterSlice";
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  function handleIncreament() {
+    dispatch(increment());
+  }
+  function handleDecreament() {
+    dispatch(decrement());
+  }
   return (
     <div>
-      <h2>hi there</h2>
+      <button onClick={handleIncreament}> +</button>
+      <h2>Count: {count}</h2>
+      <button onClick={handleDecreament}> -</button>
     </div>
   );
 }
 
 export default App;
+
+//Steps
+// Create Store
+// wrap components under providers
+// create slice
+// register reducer in slice
